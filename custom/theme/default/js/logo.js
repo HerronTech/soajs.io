842575e0d667dd8fa2c3b4b574ba2ed460394c4d
antoinehage <me@antoinehage.com>
2018-08-21 19:08:21 -0400

    top menu and carousel css

--- custom/theme/default/js/logo.js
+++ custom/theme/default/js/logo.js
@@ -2,12 +2,12 @@ jQuery(window).scroll(function() {
     var scroll = jQuery(window).scrollTop();
     
     if (scroll >= 10 && jQuery(window).width() > 767 ) {
-        jQuery(".topbar").addClass("scrolling");
-        jQuery("img.logo").attr("src", "images/logo1.png");
+        jQuery(".navbar-wrapper").addClass("scrolling");
+        jQuery("img.logo").attr("src", "config/images/soajsio-white.png");
 
     } else {
-        jQuery(".topbar").removeClass("scrolling");
-        jQuery("img.logo").attr("src", "images/logo2.png");
+        jQuery(".navbar-wrapper").removeClass("scrolling");
+        jQuery("img.logo").attr("src", "config/images/soajsio.png");
     }
 });
 
