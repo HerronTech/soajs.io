cf5fc384b111975d7493b1f0c8563e207546906a
antoinehage <me@antoinehage.com>
2018-08-22 09:19:42 -0400

    css and layout with product text

--- custom/theme/default/js/logo.js
+++ custom/theme/default/js/logo.js
@@ -2,11 +2,11 @@ jQuery(window).scroll(function() {
     var scroll = jQuery(window).scrollTop();
     
     if (scroll >= 10){//} && jQuery(window).width() > 767 ) {
-        jQuery(".navbar-wrapper").addClass("scrolling");
+        jQuery(".navbar").addClass("scrolling");
         jQuery("img.logo").attr("src", "config/images/soajsio-white.png");
 
     } else {
-        jQuery(".navbar-wrapper").removeClass("scrolling");
+        jQuery(".navbar").removeClass("scrolling");
         jQuery("img.logo").attr("src", "config/images/soajsio.png");
     }
 });
