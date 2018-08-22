f9c51dd410a005bc2e1c157a3a105abccf8965e7
antoinehage <me@antoinehage.com>
2018-08-22 11:17:43 -0400

    made inner page dynamic

--- custom/modules/store/controller.js
+++ custom/modules/store/controller.js
@@ -4,7 +4,8 @@ storeApp.controller('storeCtrl', ['$scope', function ($scope) {
 	
 	let innerPage = {
 		header: "SOAJS Store",
-		slogan: "Browse & Download Recipes and templates"
+		slogan: "Browse & Download Recipes and templates",
+        image: "custom/modules/store/images/store.jpg"
 	};
 	
 	$scope.updateParentScope('innerPage', innerPage);
